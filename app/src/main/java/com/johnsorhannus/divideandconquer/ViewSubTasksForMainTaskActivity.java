b4eb15325d203cb8983c9379bc1e94b4bc1edf07
John Sorhannus <jsorhannus@gmail.com>
2020-01-25 14:21:31 -0500

    *Added layout files for ViewSubTasksForMainTaskActivity

--- app/src/main/java/com/johnsorhannus/divideandconquer/ViewSubTasksForMainTaskActivity.java
+++ app/src/main/java/com/johnsorhannus/divideandconquer/ViewSubTasksForMainTaskActivity.java
@@ -3,6 +3,7 @@ package com.johnsorhannus.divideandconquer;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
 
 public class ViewSubTasksForMainTaskActivity extends AppCompatActivity {
@@ -11,6 +12,15 @@ public class ViewSubTasksForMainTaskActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         String name = getIntent().getExtras().getString("Name");
-        Toast.makeText(this, name, Toast.LENGTH_LONG).show();
+        setContentView(R.layout.view_maintask);
+
+        //Display toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Set title of toolbar
+        setTitle(R.string.view_maintask);
+
+        //Toast.makeText(this, name, Toast.LENGTH_LONG).show();
     }
 }
